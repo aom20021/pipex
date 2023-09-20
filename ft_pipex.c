8d2c07292099ef569c8176288ee9a3c2ba5b0ed6
Andres Ollero Morales <anollero@c2r19s5.42madrid.com>
2023-09-20 12:59:02 +0200

    freed the things that needed to be freed

--- ft_pipex.c
+++ ft_pipex.c
@@ -6,12 +6,25 @@
 /*   By: anollero <anollero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 18:34:35 by anollero          #+#    #+#             */
-/*   Updated: 2023/09/19 19:14:55 by anollero         ###   ########.fr       */
+/*   Updated: 2023/09/20 12:58:21 by anollero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_pipex.h"
 #include "include/libft.h"
+
+void	ft_free_split(char **tofree)
+{
+	int	count;
+
+	if (tofree != NULL)
+	{
+		count = 0;
+		while (tofree[count] != NULL)
+			free(tofree[count++]);
+		free(tofree);
+	}
+}
 
 /**
  * Abre los archivos de pipex en sus respectivos modos
@@ -28,11 +41,11 @@ void	ft_files(char const *argv[], t_pipex_info *info)
 		info->file1 = -1;
 	}
 	if (!access(argv[4], F_OK) && !access(argv[4], W_OK))
-		info->file2 = open(argv[4], O_WRONLY);
+		info->file2 = open(argv[4], O_WRONLY | O_TRUNC);
 	else if (!access(argv[4], F_OK))
 	{
 		perror("no se pudo abrir el fichero de salida");
-		info->file1 = -1;
+		info->file2 = -1;
 	}
 	else
 		info->file2 = creat(argv[4], 0777);
@@ -64,9 +77,7 @@ char	*command_return(char **splitted, char *command_0, int found)
 	count = 0;
 	if (!found)
 	{
-		while (splitted[count] != NULL)
-			free(splitted[count++]);
-		free(splitted);
+		ft_free_split(splitted);
 		return (NULL);
 	}
 	else
@@ -107,6 +118,7 @@ void	ft_commands(char const *argv[], char *envp[], t_pipex_info *info)
 {
 	int		count;
 	void	*path;
+	char	**path_split;
 
 	count = 0;
 	while (envp[count] != NULL)
@@ -115,16 +127,18 @@ void	ft_commands(char const *argv[], char *envp[], t_pipex_info *info)
 			path = envp[count] + 5;
 		count++;
 	}
+	path_split = ft_split(path, ':');
 	info->args1 = ft_split(argv[2], ' ');
 	info->args2 = ft_split(argv[3], ' ');
 	if (info->args1[0] == NULL || !ft_strncmp(info->args1[0], "./", 2))
 		info->command1 = info->args1[0];
 	else
-		info->command1 = ft_command(ft_split(path, ':'), info->args1);
+		info->command1 = ft_command(path_split, info->args1);
 	if (info->args2[0] == NULL || !ft_strncmp(info->args2[0], "./", 2))
 		info->command2 = info->args2[0];
 	else
-		info->command2 = ft_command(ft_split(path, ':'), info->args2);
+		info->command2 = ft_command(path_split, info->args2);
+	ft_free_split(path_split);
 }
 
 /**
@@ -139,6 +153,16 @@ void	ft_comm_files(char const *argv[], char *envp[], t_pipex_info *info)
 	info->pipeout = inout[0];
 	ft_files(argv, info);
 	ft_commands(argv, envp, info);
+	if (info->command1 == NULL)
+	{
+		write(0, "El primer comando no existe\n", 28);
+		info->args1 = NULL;
+	}
+	if (info->command2 == NULL)
+	{
+		write(0, "El segundo comando no existe\n", 29);
+		info->args2 = NULL;
+	}
 }
 
 /**
@@ -155,14 +179,15 @@ int	ft_execve_first(char *envp[], t_pipex_info *info)
 		dup(info->file1);
 		close(1);
 		dup(info->pipein);
+		close(info->file2);
+		close(info->pipeout);
 		execve(info->command1, info->args1, envp);
 		perror("el comando no existe");
 		exit(0);
 	}
 	if (pid == -1)
 		exit(0);
-	close(info->pipein);
-	close(info->file1);
+	free(info->command1);
 	return (pid);
 }
 
@@ -188,8 +213,7 @@ int	ft_execve_second(char *envp[], t_pipex_info *info)
 	}
 	if (pid == -1)
 		exit(0);
-	close(info->pipeout);
-	close(info->file2);
+	free(info->command2);
 	return (pid);
 }
 
@@ -203,13 +227,20 @@ int	main(int argc, char const *argv[], char *envp[])
 	if (argc != 5)
 		exit(0);
 	ft_comm_files(argv, envp, info);
-	if (info->file1 != -1 && info->args1[0] != NULL)
+	if (info->file1 != -1 && info->args1 != NULL && info->args1[0] != NULL)
 		pid1 = ft_execve_first(envp, info);
-	if (info->file2 != -1 && info->args2[0] != NULL)
+	close(info->pipein);
+	close(info->file1);
+	if (info->file2 != -1 && info->args2 != NULL && info->args2[0] != NULL)
 		pid2 = ft_execve_second(envp, info);
-	if (info->file1 != -1 && info->args1[0] != NULL)
+	close(info->pipeout);
+	close(info->file2);
+	if (info->file1 != -1 && info->args1 != NULL && info->args1[0] != NULL)
 		waitpid(pid1, NULL, 0);
-	if (info->file2 != -1 && info->args2[0] != NULL)
+	if (info->file2 != -1 && info->args2 != NULL && info->args2[0] != NULL)
 		waitpid(pid2, NULL, 0);
+	ft_free_split(info->args1);
+	ft_free_split(info->args2);
+	free(info);
 	return (0);
 }
